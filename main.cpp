35df58a92a5a994ecacd5c86197748edfae4abc5
Sadeg02 <mich2002m@gmail.com>
2023-12-21 23:03:48 +0100

    proba

--- main.cpp
+++ main.cpp
@@ -11,12 +11,16 @@ int main() {
         std::getline(std::cin, rozkaz);
         std::istringstream linia(rozkaz);
         if(rozkaz == "q"){
+            std::cout << "Rozłączanie"<<std::endl;
             break;
         }
         char figura;
-        int stareX, stareY, noweX, noweY;
-        if (linia >> figura >> stareX >> stareY >> noweX >> noweY) {
-            // Tutaj możesz dodać dodatkową logikę, np. sprawdzenie poprawności ruchu
+        std::string starePole, nowePole;
+        if (linia >> figura >> starePole >> nowePole) {
+            int stareX = stoi(starePole.substr(0, 1));
+            int stareY = stoi(starePole.substr(1, 1));
+            int noweX = stoi(nowePole.substr(0, 1));
+            int noweY = stoi(nowePole.substr(1, 1));
             pion(szachownica, figura, stareX, stareY, noweX, noweY);
         } else {
             std::cout << "Bledny format ruchu. Poprawny format: [figura] [stareX] [stareY] [noweX] [noweY]" << std::endl;
